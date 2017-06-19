199497f170b82d7056790e115741d4c7d3b48e6f
Joe Hall <joe.hall@pod-point.com>
2017-06-19 13:45:26 +0100

    Fixed use of quote marks

--- tests/src/Functional/ViewFormComponentsTest.php
+++ tests/src/Functional/ViewFormComponentsTest.php
@@ -23,14 +23,14 @@ class ViewFormComponentsTest extends TestCase {
 
         $renderedView = $this->renderBladeView('checkbox', $data);
 
-        $this->assertTrue(str_contains($renderedView, "<input"));
+        $this->assertTrue(str_contains($renderedView, '<input'));
         $this->assertTrue(str_contains($renderedView, "id=\"{$data['name']}\""));
         $this->assertTrue(str_contains($renderedView, "name=\"{$data['name']}\""));
         $this->assertTrue(str_contains($renderedView, "<span class=\"form__label\">{$data['labelText']}</span>"));
         $this->assertTrue(str_contains($renderedView, "<label class=\"checkbox {$data['labelClass']}\" for=\"{$data['name']}\">"));
         $this->assertTrue(str_contains($renderedView, "value=\"{$data['values'][0]}\""));
         $this->assertTrue(str_contains($renderedView, "<span>{$data['options']['option1']}</span>"));
-        $this->assertTrue(str_contains($renderedView, "disabled"));
+        $this->assertTrue(str_contains($renderedView, 'disabled'));
     }
 
     /**
@@ -52,7 +52,7 @@ class ViewFormComponentsTest extends TestCase {
 
         $renderedView = $this->renderBladeView('checkbox', $data);
 
-        $this->assertTrue(str_contains($renderedView, "<input"));
+        $this->assertTrue(str_contains($renderedView, '<input'));
         $this->assertTrue(str_contains($renderedView, "<span class=\"form__label\">{$data['labelText']}</span>"));
         foreach($data['options'] as $option => $optionValue) {
             $this->assertTrue(str_contains($renderedView, "id=\"{$data['name']}[{$option}]\""));
@@ -61,8 +61,8 @@ class ViewFormComponentsTest extends TestCase {
             $this->assertTrue(str_contains($renderedView, "value=\"{$option}\""));
             $this->assertTrue(str_contains($renderedView, "<span>{$optionValue}</span>"));
         }
-        $this->assertTrue(substr_count($renderedView, "checked") === 1);
-        $this->assertTrue(str_contains($renderedView, "disabled"));
+        $this->assertTrue(substr_count($renderedView, 'checked') === 1);
+        $this->assertTrue(str_contains($renderedView, 'disabled'));
     }
 
     /**
@@ -81,14 +81,14 @@ class ViewFormComponentsTest extends TestCase {
 
         $renderedView = $this->renderBladeView('input', $data);
 
-        $this->assertTrue(str_contains($renderedView, "<input"));
+        $this->assertTrue(str_contains($renderedView, '<input'));
         $this->assertTrue(str_contains($renderedView, "id=\"{$data['name']}\""));
         $this->assertTrue(str_contains($renderedView, "name=\"{$data['name']}\""));
         $this->assertTrue(str_contains($renderedView, "type=\"{$data['type']}\""));
         $this->assertTrue(str_contains($renderedView, "<label class=\"form__label {$data['labelClass']}\" for=\"{$data['name']}\">{$data['labelText']}</label>"));
         $this->assertTrue(str_contains($renderedView, "value=\"{$data['value']}\""));
         $this->assertTrue(str_contains($renderedView, "placeholder=\"{$data['placeholder']}\""));
-        $this->assertTrue(str_contains($renderedView, "disabled"));
+        $this->assertTrue(str_contains($renderedView, 'disabled'));
     }
 
     /**
@@ -104,11 +104,11 @@ class ViewFormComponentsTest extends TestCase {
 
         $renderedView = $this->renderBladeView('file-upload', $data);
 
-        $this->assertTrue(str_contains($renderedView, "<input"));
+        $this->assertTrue(str_contains($renderedView, '<input'));
         $this->assertTrue(str_contains($renderedView, "id=\"{$data['name']}\""));
         $this->assertTrue(str_contains($renderedView, "name=\"{$data['name']}\""));
         $this->assertTrue(str_contains($renderedView, "<label class=\"form__label {$data['labelClass']}\" for=\"{$data['name']}\">{$data['labelText']}</label>"));
-        $this->assertTrue(str_contains($renderedView, "disabled"));
+        $this->assertTrue(str_contains($renderedView, 'disabled'));
     }
 
     /**
@@ -129,7 +129,7 @@ class ViewFormComponentsTest extends TestCase {
 
         $renderedView = $this->renderBladeView('radio', $data);
 
-        $this->assertTrue(str_contains($renderedView, "<input"));
+        $this->assertTrue(str_contains($renderedView, '<input'));
         foreach($data['options'] as $option => $optionValue) {
             $this->assertTrue(str_contains($renderedView, "<label class=\"form__label radio {$data['labelClass']}\" for=\"{$data['name']}_{$option}\">"));
             $this->assertTrue(str_contains($renderedView, "id=\"{$data['name']}_{$option}\""));
@@ -137,8 +137,8 @@ class ViewFormComponentsTest extends TestCase {
             $this->assertTrue(str_contains($renderedView, "value=\"{$option}\""));
             $this->assertTrue(str_contains($renderedView, "<span>{$optionValue}</span>"));
         }
-        $this->assertTrue(substr_count($renderedView, "checked") === 1);
-        $this->assertTrue(str_contains($renderedView, "disabled"));
+        $this->assertTrue(substr_count($renderedView, 'checked') === 1);
+        $this->assertTrue(str_contains($renderedView, 'disabled'));
     }
 
     /**
@@ -159,14 +159,14 @@ class ViewFormComponentsTest extends TestCase {
 
         $renderedView = $this->renderBladeView('select', $data);
 
-        $this->assertTrue(str_contains($renderedView, "<select"));
+        $this->assertTrue(str_contains($renderedView, '<select'));
         foreach($data['options'] as $option => $optionValue) {
             $this->assertTrue(str_contains($renderedView, "<select class=\"form__control\" id=\"{$data['name']}\" name=\"{$data['name']}\""));
             $this->assertTrue(str_contains($renderedView, "value=\"{$option}\""));
             $this->assertTrue(str_contains($renderedView, "{$optionValue}"));
         }
-        $this->assertTrue(substr_count($renderedView, "selected") === 1);
-        $this->assertTrue(str_contains($renderedView, "disabled"));
+        $this->assertTrue(substr_count($renderedView, 'selected') === 1);
+        $this->assertTrue(str_contains($renderedView, 'disabled'));
     }
 
     /**
@@ -185,12 +185,12 @@ class ViewFormComponentsTest extends TestCase {
 
         $renderedView = $this->renderBladeView('textarea', $data);
 
-        $this->assertTrue(str_contains($renderedView, "<textarea"));
+        $this->assertTrue(str_contains($renderedView, '<textarea'));
         $this->assertTrue(str_contains($renderedView, "id=\"{$data['name']}\""));
         $this->assertTrue(str_contains($renderedView, "name=\"{$data['name']}\""));
         $this->assertTrue(str_contains($renderedView, "<label class=\"form__label {$data['labelClass']}\" for=\"{$data['name']}\">{$data['labelText']}</label>"));
         $this->assertTrue(str_contains($renderedView, "{$data['value']}"));
         $this->assertTrue(str_contains($renderedView, "placeholder=\"{$data['placeholder']}\""));
-        $this->assertTrue(str_contains($renderedView, "disabled"));
+        $this->assertTrue(str_contains($renderedView, 'disabled'));
     }
 }
