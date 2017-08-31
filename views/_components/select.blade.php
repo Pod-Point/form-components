b24d287c7cadb2271197ca85009a66c63e109f8a
Joe Hall <joe.hall@pod-point.com>
2017-08-31 18:13:48 +0100

    Fix so dropdowns remember prev submitted value

--- views/_components/select.blade.php
+++ views/_components/select.blade.php
@@ -17,7 +17,7 @@
     <div class="select-wrapper">
         <select class="form__control" id="{{ $name }}" name="{{ $name }}" {{ isset($disabled) ? 'disabled' : '' }}>
             @foreach ($options as $key => $option)
-                <option value="{{ $key }}" {{ $key === $value ? 'selected' : '' }}>
+                <option value="{{ $key }}" {{ $key == $value ? 'selected' : '' }}>
                     {{ $option }}
                 </option>
             @endforeach
