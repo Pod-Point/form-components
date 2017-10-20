6ebbdc9ad3aa37bdb40156b7e37c8d2cb767f261
Ben Speakman <ben@3sq.re>
2017-10-20 10:22:13 +0100

    Render form errors (#19)
    * Render form errors
    
    * Add badge

--- views/_components/form-group.blade.php
+++ views/_components/form-group.blade.php
@@ -2,6 +2,12 @@
     use PodPoint\FormComponents\FormComponentsServiceProvider;
 
     $classes['formGroup'] = $classes['formGroup'] ?? FormComponentsServiceProvider::FORM_GROUP_DEFAULT_CLASS;
+    $classes['formGroupError'] = $classes['formGroupError'] ?? FormComponentsServiceProvider::FORM_GROUP_ERROR_CLASS;
+    $classes['formErrorSpan'] = $classes['formErrorSpan'] ?? FormComponentsServiceProvider::FORM_ERROR_SPAN_CLASS;
+
+    if ($errors->has($name)) {
+        $classes['formGroup'] = $classes['formGroup'] . ' ' . $classes['formGroupError'];
+    }
 @endphp
 
 <div class="{{ $classes['formGroup'] }}">
@@ -9,5 +15,11 @@
     @yield('label')
 
     @yield('input')
+
+    @if($errors->has($name))
+        @foreach($errors->get($name) as $message)
+            <span class="{{ $classes['formErrorSpan'] }}">{{ $message }}</span>
+        @endforeach
+    @endif
 
 </div>
